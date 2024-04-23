b6551b8510c090c4d197d6f3d89f94eb1a5d07aa
haatajaj <joel.haataja@gmail.com>
2024-04-24 01:36:28 +0300

    Backend server template with https & http ports

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import https from "https";
+import fs from "fs";
+
+import {PORT} from "./config.js";
+
+const options = {
+    key: fs.readFileSync('.secret/selfsigned.key'),
+    cert: fs.readFileSync('.secret/selfsigned.crt')
+};
+
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Login')
+});
+
+const server = https.createServer(options, app)
+server.listen(PORT, () => {
+    console.log('HTTPS Server started on port ' + PORT)
+});
+
+app.listen(3001, () => {
+    console.log('HTTP Server started on port ' + 3001)
+});
