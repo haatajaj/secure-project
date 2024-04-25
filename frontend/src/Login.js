07d26eb9e563f4901307e801d9fe896fdef2d572
haatajaj <joel.haataja@gmail.com>
2024-04-25 05:08:53 +0300

    Login form and some input validation

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,13 +1,65 @@
+import { useState } from "react";
 import React from "react";
 import NavBar from "./NavBar";
 
 
 const Login = () => {
-    //const onButtonClick = () => {}
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const [usernameErr, setUsernameErr] = useState("")
+    const [passwordErr, setPasswordErr] = useState("")
+
+
+
+
+    const onButtonClick = () => {
+        setUsernameErr("")
+        setPasswordErr("")
+
+        if(username === "") {
+            setUsernameErr("Enter your username")
+        }
+        else if(!/^[0-9A-Za-z]{4,12}$/.test(username)) {
+            setUsernameErr("Enter valid username")
+        }
+        if(password === "") {
+            setPasswordErr("Enter your password")
+        }
+        else if(!/^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z]).{10,40}$/.test(password)) {
+            setPasswordErr("Enter valid password")
+        }
+
+    }
+
     return (
-        <div>
+        <div className="main">
             <NavBar />
-            <h1>Login</h1>
+            <div className="title">
+                <h1>Login</h1>
+            </div>
+            <div className="inputDiv1">
+                <input
+                    value={username}
+                    placeholder="username"
+                    onChange={(x) => setUsername(x.target.value)}
+                />
+                <br/>
+                <label>{usernameErr}</label>
+            </div>
+            <div className="inputDiv2">
+                <input 
+                    value={password}
+                    type="password"
+                    placeholder="password"
+                    onChange={(x) => setPassword(x.target.value)}
+                />
+                    <br/>
+                    <label>{passwordErr}</label>
+            </div>
+            <div className="inputDiv3">
+                <input className="inputButton" type="button" onClick={onButtonClick} value={"Login"} />
+            </div>
         </div>
     );
 }
