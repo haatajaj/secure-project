ddd8c526eec6875d6dc2f481a9013ee9abbb0921
haatajaj <joel.haataja@gmail.com>
2024-04-25 05:08:13 +0300

    Page routes for frontend

---
+++ frontend/src/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+
+const Login = () => {
+    //const onButtonClick = () => {}
+    return (
+        <div>
+            <NavBar />
+            <h1>Login</h1>
+        </div>
+    );
+}
+export default Login;
---
+++ frontend/src/Register.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+
+const Register = () => {
+    //const onButtonClick = () => {}
+    return (
+        <div>
+            <NavBar />
+            <h1>Register</h1>
+        </div>
+    );
+}
+export default Register;
