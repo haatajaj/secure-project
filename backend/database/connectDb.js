e983a42642af8ca1d2ac865ee4ac295d01be7361
haatajaj <joel.haataja@gmail.com>
2024-04-26 10:56:35 +0300

    Updated port and removed console.log

--- backend/database/connectDb.js
+++ backend/database/connectDb.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 //const connectDb = async () => {
 async function connectDb() {
-    console.log(process.env.DB_URL);
+    //console.log(process.env.DB_URL);
     mongoose.connect(process.env.DB_URL)
-    .then(() => {console.log("Connected to MongoDB Atlas");})
+    .then(() => {console.log("Connected: MongoDB Atlas");})
     .catch((err) => {console.log(err);});
 
 
