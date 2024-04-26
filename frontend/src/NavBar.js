51fe78682ca7a43ce401fd17f0ea680878da501e
haatajaj <joel.haataja@gmail.com>
2024-04-26 10:50:03 +0300

    Navbar changed to row and added css

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,18 +1,13 @@
 import {Link} from "react-router-dom";
+import "./NavBar.css"
 
 const NavBar = () => {
     return (
         <nav>
             <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
             </ul>
         </nav>
     );
