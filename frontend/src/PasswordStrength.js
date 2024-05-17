9cf9e3e4ebeee1f70f431ea25cdfb09db850167c
haatajaj <joel.haataja@gmail.com>
2024-05-17 15:45:36 +0300

    Added password strength meter

---
+++ frontend/src/PasswordStrength.js
@@ -0,0 +1,21 @@
+import {passwordStrength} from 'check-password-strength';
+import './PasswordStrength.css'
+
+
+
+const PasswordStrength = (prop) => {
+    let strength = passwordStrength(prop.password);
+
+    let widths = ["1%", "33%", "66%", "100%"]; 
+    let colors = ["#FF0000", "#FF4500", "#FFFF00", "#008000"]; 
+
+
+    console.log(strength)
+    return (
+        <div className="strengthContainer"> 
+            <div className="strengthBar" style={
+                {width: widths[strength.id], "background-color": colors[strength.id]}} /> 
+        </div>
+    );
+}
+export default PasswordStrength;
